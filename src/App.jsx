1171c1cd7e22e6e5a789619c6dad410a9ef36688
Kuldeep Singh <148358556+Kuldeep-W3B@users.noreply.github.com>
2025-10-01 16:05:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import CoinDetail from "./pages/CoinDetail";
+import CoinDetail from "./pages/CoinDetail.jsx";
 
 function App() {
   return (
